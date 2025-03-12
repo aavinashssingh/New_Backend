60fe457dc70891595dc5474ac4cee58f421c7ba5
Avinash <aka.avinash.singh@gmail.com>
2025-03-12 11:38:10 +0530

    Added changes

--- app/v1/redisCient/rediscCient.js
+++ app/v1/redisCient/rediscCient.js
@@ -1,20 +1,26 @@
-require("dotenv").config();
-const redis = require("redis");
+require('dotenv').config();
+const redis = require('redis');
 
 const client = redis.createClient({
-  host: process.env.REDIS_HOST, 
-  port: process.env.REDIS_PORT,
-  // password: process.env.REDIS_PASSWORD 
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
 });
 
-client.on("connect", () => {
-  console.log("Connected to Redis");
+client.on('error', (err) => {
+  console.error('Redis Client Error', err);
 });
 
-client.on("error", (err) => {
-  console.error("Redis error:", err);
-});
-
-client.connect(); 
+(async () => {
+  try {
+    await client.connect();
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+  }
+})();
 
 module.exports = client;
